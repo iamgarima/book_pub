ec5b39d2a14fdf334cadd4ec62e242f47ab33b07
unknown <garima.kamboj@icloud.com>
2018-01-26 23:37:07 -0800

    Add header

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
+import Header from './components/Header';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div><Header /><App /></div>, document.getElementById('root'));
 registerServiceWorker();
